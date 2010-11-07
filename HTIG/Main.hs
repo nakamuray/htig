0e9c1487f03044e20393ecedf2257d5d6e17829e
NAKAMURA Yoshitaka <arumakanoy@gmail.com>
2010-11-07 21:24:33 +0900

    parse command line arguments to control Dyre/GHC

--- HTIG/Main.hs
+++ HTIG/Main.hs
@@ -4,6 +4,8 @@ module HTIG.Main
 
 import Control.Applicative ((<$>))
 import Data.Time (getCurrentTime)
+import System.Environment (getArgs)
+import System.Exit (exitSuccess, exitFailure)
 import System.FilePath ((</>))
 import System.IO (stderr, hPutStrLn)
 
@@ -30,14 +32,40 @@ realMain cfg = do
         s = Server h p (return g) (return newEmptyState) (return . const HSession)
     runServer s
 
+type Params = Dyre.Params HConfig
+
+defaultParams :: Params
+defaultParams = Dyre.defaultParams
+        { Dyre.projectName = s appName
+        , Dyre.configDir   = Just getHTIGDir
+        , Dyre.cacheDir    = Just ((</> "build") <$> getHTIGDir)
+        , Dyre.realMain    = realMain
+        , Dyre.showError   = showError
+        -- "-i" is relative path from "~/.htig/build"
+        , Dyre.ghcOpts     = ["-O2", "-i../lib"]
+        }
+
+parseArgs :: IO Params
+parseArgs = parseArgs' defaultParams =<< getArgs
+
+parseArgs' :: Params -> [String] -> IO Params
+parseArgs' p []                      = return p
+parseArgs' _ ("--help":_)            = printHelp >> exitSuccess
+parseArgs' _ ("--version":_)         = putStrLn (s appName ++ "-" ++ s appVersion) >> exitSuccess
+parseArgs' p ("--force-reconf":args) = parseArgs' p { Dyre.forceRecomp = True } args
+parseArgs' p ("--":args)             = return p { Dyre.ghcOpts = Dyre.ghcOpts p ++ args }
+parseArgs' _ (arg:_)                 = hPutStrLn stderr ("Error: unknown option: " ++ arg) >> exitFailure
+
+printHelp :: IO ()
+printHelp = putStrLn $ unlines $
+    [ "Usage: " ++ s appName ++ " [OPTION] [-- GHC Options]"
+    , "Options:"
+    , "  --help            print this message"
+    , "  --version         print the version number"
+    , "  --force-reconf    force recompile your ~/.htig/htig.hs"
+    ]
+
 htig :: HConfig -> IO ()
-htig = Dyre.wrapMain $ Dyre.defaultParams
-    { Dyre.projectName = s appName
-    , Dyre.configDir   = Just getHTIGDir
-    , Dyre.cacheDir    = Just ((</> "build") <$> getHTIGDir)
-    , Dyre.realMain    = realMain
-    , Dyre.showError   = showError
-    , Dyre.hidePackages = ["monads-fd", "monads-tf"]
-    -- "-i" is relative path from "~/.htig/build"
-    , Dyre.ghcOpts     = ["-O2", "-i../lib"]
-    }
+htig cfg = do
+    params <- parseArgs
+    Dyre.wrapMain params cfg
